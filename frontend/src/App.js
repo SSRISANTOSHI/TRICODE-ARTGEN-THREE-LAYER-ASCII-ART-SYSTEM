c6c07ebd094e14cf9fb01970a66e629828608d45
SSRISANTOSHI <s.satvika2005@gmail.com>
2026-06-17 18:57:35 +0530

    Add frontend source code

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AsciiArtGenerator from './components/AsciiArtGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <AsciiArtGenerator />
+    </div>
+  );
+}
+
+export default App;
